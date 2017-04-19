20d62d2cb5490a328cb7bc6ab42a4eea7ba66899
ankita <ankitabenoza@Ankitabens-MacBook-Pro.local>
2017-04-18 23:50:37 -0400

    Pillow Lib

--- pillow.py
+++ pillow.py
@@ -1,9 +1,26 @@
 from PIL import Image
 
 img=Image.open("flower.jpg")
-
 print(img.size)
-
 print(img.format)
-
 img.show()
+
+# crop the image
+
+area=(100,100,400,500)
+cropped_img=img.crop(area)
+cropped_img.show()
+
+# combine two image
+flower=Image.open("flower.jpg")
+bear=Image.open("bear.gif")
+combineArea=(100,100,500,500)
+flower.paste(bear)
+flower.show()
+
+# getting individual channel color
+img1=Image.open("flower.jpg")
+r,g,b=img1.split()
+r.show()
+g.show()
+b.show()
